59024e0668d58f0f15d02ec2156223c240bda72b
ZhangShen <ZhangShen@shanghaitech.edu.cn>
2023-09-03 16:01:30 +0800

    fix the bug in clean.sh

--- clean.sh
+++ clean.sh
@@ -3,7 +3,6 @@
 find "./vivado" -mindepth 1 ! -name "readme.txt" -delete
 find "./" -name "*vivado*.log" -type f -delete
 find "./" -name "*vivado*.jou" -type f -delete
-find "./" -name ".Xil" -type f -delete
 find "./" -type d -name "*.Xil" -exec rm -rf {} +
 
 echo "清理完成"
