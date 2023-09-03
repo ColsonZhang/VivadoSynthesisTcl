098bcf6e731113fce3dbfda257a02cb31302dffb
ZhangShen <ZhangShen@shanghaitech.edu.cn>
2023-09-03 15:59:01 +0800

    add force to create vivado project

--- tcl/scripts/vivado.tcl
+++ tcl/scripts/vivado.tcl
@@ -1,5 +1,5 @@
 source "function.tcl"
 
 proc create_project_with_board {project_name project_path board_part} {
-    create_project $project_name $project_path -part $board_part
+    create_project -force $project_name $project_path -part $board_part
 }
