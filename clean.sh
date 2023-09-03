ec5da7b8e041fe0330d89be11834b1b5e840280e
ZhangShen <ZhangShen@shanghaitech.edu.cn>
2023-09-03 14:16:32 +0800

    optimize the clean.sh

--- clean.sh
+++ clean.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-
-directory="./vivado"
-find "$directory" -mindepth 1 ! -name "readme.txt" -delete
+find "./vivado" -mindepth 1 ! -name "readme.txt" -delete
+find "./" -name "*vivado*.log" -type f -delete
+find "./" -name "*vivado*.jou" -type f -delete
+find "./" -name ".Xil" -type f -delete
+find "./" -type d -name "*.Xil" -exec rm -rf {} +
 
 echo "清理完成"
